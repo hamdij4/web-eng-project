12a9c58602eec039c1a39400137980ab71199606
freund-admin <hr@freund.ba>
2019-12-05 15:51:31 +0100

    Minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Badge } from 'reactstrap';
+import {
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Button
+} from "reactstrap";
 
 class App extends React.Component {
   state = {
-    data: null
+    username: null,
+    formInput: 'ex. Tarik1234'
   };
 
   componentDidMount() {
-    // Call our fetch function below once the component mounts
-    this.callBackendAPI()
-      .then(res => { this.setState({ data: res[0].username }) })
+    this.getUsername()
+      .then(res => { this.setState({ username: res[0].username }) })
       .catch(err => { console.log(err) });
   }
-  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  callBackendAPI = async () => {
+  getUsername = async () => {
     const response = await fetch('customer/user/hamdija1234');
     const body = await response.json();
 
@@ -24,17 +30,30 @@ class App extends React.Component {
     }
     return body;
   };
-
+  handleForm(event){
+    event.preventDefault()
+  }
+  updateField(event){
+    this.setState({formInput : event.target.value}, () => console.log(this.state.formInput))
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-<Badge color="success" pill>{this.state.data}</Badge>{" "}
+          <Badge color="success" pill>{this.state.username}</Badge>{" "}
+          <form>
+            <FormGroup>
+              <Label for="username">Get user</Label>
+              <Input type="text" name="username" placeholder="" value={this.state.formInput}
+              onChange={this.updateField.bind(this)}/>
+              <Button className="btn-round" color="primary" style={{margin: "10px"}} type="button" onClick={this.handleForm}>
+                Search
+</Button>
+            </FormGroup>
+          </form>
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        // Render the newly fetched data inside of this.state.data 
-        <p className="App-intro">{this.state.data}</p>
       </div>
     );
   }
