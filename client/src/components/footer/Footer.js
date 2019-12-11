cf558f10a94f0ac3641492f3da56502332399a5e
freund-admin <hr@freund.ba>
2019-12-11 13:48:11 +0100

    more css separating

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -9,15 +9,15 @@ function Footer(){
         <div className="footer-container">
             <Container>
                 <Row>
-                <Col>
+                <Col lg={4} md={4} sm={12} xs={12}>
                 <h3>About Us</h3>
                 <p>Lorem ipsum dolor sit amet, amor omnia vincit. Lorem ipsum dolor sit amet, amor omnia vincit</p>
                 </Col>
-                <Col>
+                <Col lg={4} md={4} sm={12} xs={12}>
                 <h3>Find Us</h3>
                 <p>We're currently operating only in Sarajevo, BiH. Samo 387 71000 brate moj !!!</p>
                 </Col>
-                <Col>
+                <Col lg={4} md={4} sm={12} xs={12}>
                 <h3>Contact Us</h3>
                 <p>Telephone: +387 53 53 53</p>
                 </Col></Row>
--- client/src/components/landing-user-reviews/LandingUserReviews.js
+++ client/src/components/landing-user-reviews/LandingUserReviews.js
@@ -1,65 +1,56 @@
 
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
-import { Card, CardImg, CardBody, CardTitle, CardText, Badge} from 'reactstrap';
+import { Card, CardImg, CardBody, CardTitle, CardText, Badge } from 'reactstrap';
+import '../../components/landing-user-reviews/landing-user-reviews.css'
 import Image from 'react-bootstrap/Image'
 
-const componentStyle = {
-    maxHeight : "130vh",
-    minHeight : "900px",
-    backgroundColor : "rgba(255, 167, 59, 1)",
-    width : "100vw",
-    padding: "50px"
-};
 
-const cardStyle = {
-    height: "50%",
-    minHeight: "50%",
-    width: "auto",
-    minWidth: "50%"
-}
-function LandingReviews(){
+function LandingReviews() {
 
     return (
-            <>
-            <div className="landing-review-container" style={componentStyle}>
-                <h2 style={{marginBottom: "75px"}}>What they say about us</h2>
+        <>
+            <div className="review-container">
+                <h2 style={{ marginBottom: "75px" }}>What they say about us</h2>
                 <Container>
                     <Row >
-                        <Col xs={12} sm = {12} lg={4} md={4}><Card style={cardStyle, {marginBottom: "30px"}}>
-                            <CardImg top src={require("../../assets/img/DefaultProfile1.jpg")} alt="..."  />
-                            <CardTitle>
-<Badge color="warning" pill>Ed Sheeran</Badge>{" "}
-                            </CardTitle>
-                            <CardBody>
-                                <CardText><h5>Customer Service done right!</h5></CardText>
-                            </CardBody>
-                        </Card>
+                        <Col xs={12} sm={12} lg={4} md={4} style={{marginBottom: "15px"}}>
+                            <Card className="review-card">
+                                <CardImg top src={require("../../assets/img/DefaultProfile1.jpg")} alt="..." />
+                                <CardTitle>
+                                    <Badge color="warning" pill>Ed Sheeran</Badge>{" "}
+                                </CardTitle>
+                                <CardBody>
+                                    <CardText><h5>Customer Service done right!</h5></CardText>
+                                </CardBody>
+                            </Card>
                         </Col>
-                        <Col xs={12} sm = {12} lg={4} md={4}><Card style={cardStyle, {marginBottom: "30px"}}>
-                            <CardImg top src={require("../../assets/img/DefaultProfile2.jpg")} alt="..."  />
-                            <CardTitle>
-<Badge color="warning" pill>Ed Sheeran</Badge>{" "}
-                            </CardTitle>
-                            <CardBody>
-                                <CardText><h5>Customer Service done right!</h5></CardText>
-                            </CardBody>
-                        </Card>
+                        <Col xs={12} sm={12} lg={4} md={4} style={{marginBottom: "15px"}}>
+                            <Card className="review-card" >
+                                <CardImg top src={require("../../assets/img/DefaultProfile2.jpg")} alt="..." />
+                                <CardTitle>
+                                    <Badge color="warning" pill>Ed Sheeran</Badge>{" "}
+                                </CardTitle>
+                                <CardBody>
+                                    <CardText><h5>Customer Service done right!</h5></CardText>
+                                </CardBody>
+                            </Card>
                         </Col>
-                        <Col xs={12} sm = {12} lg={4} md={4}><Card style={cardStyle, {marginBottom: "30px"}}>
-                            <CardImg top src={require("../../assets/img/DefaultProfile1.jpg")} alt="..."  />
-                            <CardTitle>
-<Badge color="warning" pill>Ed Sheeran</Badge>{" "}
-                            </CardTitle>
-                            <CardBody>
-                                <CardText><h5>Customer Service done right!</h5></CardText>
-                            </CardBody>
-                        </Card>
+                        <Col xs={12} sm={12} lg={4} md={4}  style={{marginBottom: "15px"}}>
+                            <Card className="review-card">
+                                <CardImg top src={require("../../assets/img/DefaultProfile1.jpg")} alt="..." />
+                                <CardTitle>
+                                    <Badge color="warning" pill>Ed Sheeran</Badge>{" "}
+                                </CardTitle>
+                                <CardBody>
+                                    <CardText><h5>Customer Service done right!</h5></CardText>
+                                </CardBody>
+                            </Card>
                         </Col>
                     </Row>
-                    </Container>
+                </Container>
             </div>
-            </>
+        </>
     )
 }
 
