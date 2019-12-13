e37cc5f4a2729f063d08765eca77136003bb30cf
freund-admin <hr@freund.ba>
2019-12-13 14:37:26 +0100

    pagination skeleton on food menu added

--- client/src/components/order/order-menu/OrderMenu.js
+++ client/src/components/order/order-menu/OrderMenu.js
@@ -1,8 +1,10 @@
 import './order-menu.css'
 import React from "react";
 import classnames from "classnames";
-import {Container, Col, Row} from 'reactstrap'
-import { Card, CardImg, CardBody, CardTitle, CardText, Badge,CardImgOverlay} from 'reactstrap';
+import { Container, Col, Row } from 'reactstrap'
+import { Card, CardImg, CardBody, CardTitle, CardText, Badge, CardImgOverlay } from 'reactstrap';
+
+import { Pagination, PaginationItem, PaginationLink } from "reactstrap";
 
 function OrderMenuComponent() {
     return (
@@ -13,84 +15,115 @@ function OrderMenuComponent() {
                         Hamburgers
                     </h2>
                 </div>
-                <Container>
+                <Container className="card-container">
                     <Row>
-                        <Col className="food-col">
-                            <Card className = "food-card">
-                            <CardImg top src={require("../../../assets/img/Hamburger.jpg")} alt="..." />
-                            <CardTitle>
-                                Hamburger XL
+                        <Col lg={4} md={4} sm={4} className="food-col">
+                            <Card className="food-card">
+                                <CardImg top src={require("../../../assets/img/Hamburger.jpg")} alt="..." />
+                                <CardTitle>
+                                    Hamburger XL
                             </CardTitle>
-                            <CardBody>
-                                <CardText>
-                                <h6>Tomatoes, Cheese, Salad, Ground Beef</h6></CardText>
-                            </CardBody>
-                        </Card>
+                                <CardBody>
+                                    <CardText>
+                                        <h6>Tomatoes, Cheese, Salad, Ground Beef</h6></CardText>
+                                </CardBody>
+                            </Card>
                         </Col>
-                        <Col className="food-col">
-                            <Card className = "food-card">
-                            <CardImg top src={require("../../../assets/img/Hamburger.jpg")} alt="..." />
-                            <CardTitle>
-                                Hamburger XL
+                        <Col lg={4} md={4} sm={4} className="food-col">
+                            <Card className="food-card">
+                                <CardImg top src={require("../../../assets/img/Hamburger.jpg")} alt="..." />
+                                <CardTitle>
+                                    Hamburger XL
                             </CardTitle>
-                            <CardBody>
-                                <CardText>
-                                <h6>Tomatoes, Cheese, Salad, Ground Beef</h6></CardText>
-                            </CardBody>
-                        </Card>
+                                <CardBody>
+                                    <CardText>
+                                        <h6>Tomatoes, Cheese, Salad, Ground Beef</h6></CardText>
+                                </CardBody>
+                            </Card>
                         </Col>
-                        <Col className="food-col">
-                            <Card className = "food-card">
-                            <CardImg top src={require("../../../assets/img/Hamburger.jpg")} alt="..." />
-                            <CardTitle>
-                                Hamburger XL
+                        <Col lg={4} md={4} sm={4} className="food-col">
+                            <Card className="food-card">
+                                <CardImg top src={require("../../../assets/img/Hamburger.jpg")} alt="..." />
+                                <CardTitle>
+                                    Hamburger XL
                             </CardTitle>
-                            <CardBody>
-                                <CardText>
-                                <h6>Tomatoes, Cheese, Salad, Ground Beef</h6></CardText>
-                            </CardBody>
-                        </Card>
+                                <CardBody>
+                                    <CardText>
+                                        <h6>Tomatoes, Cheese, Salad, Ground Beef</h6></CardText>
+                                </CardBody>
+                            </Card>
                         </Col>
                     </Row>
                     <Row>
-                        <Col className="food-col">
-                            <Card className = "food-card">
-                            <CardImg top src={require("../../../assets/img/Hamburger.jpg")} alt="..." />
-                            <CardTitle>
-                                Hamburger XL
+                        <Col lg={4} md={4} sm={4} className="food-col">
+                            <Card className="food-card">
+                                <CardImg top src={require("../../../assets/img/Hamburger.jpg")} alt="..." />
+                                <CardTitle>
+                                    Hamburger XL
                             </CardTitle>
-                            <CardBody>
-                                <CardText>
-                                <h6>Tomatoes, Cheese, Salad, Ground Beef</h6></CardText>
-                            </CardBody>
-                        </Card>
+                                <CardBody>
+                                    <CardText>
+                                        <h6>Tomatoes, Cheese, Salad, Ground Beef</h6></CardText>
+                                </CardBody>
+                            </Card>
                         </Col>
-                        <Col className="food-col">
-                            <Card className = "food-card">
-                            <CardImg top src={require("../../../assets/img/Hamburger.jpg")} alt="..." />
-                            <CardTitle>
-                                Hamburger XL
+                        <Col lg={4} md={4} sm={4} className="food-col">
+                            <Card className="food-card">
+                                <CardImg top src={require("../../../assets/img/Hamburger.jpg")} alt="..." />
+                                <CardTitle>
+                                    Hamburger XL
                             </CardTitle>
-                            <CardBody>
-                                <CardText>
-                                <h6>Tomatoes, Cheese, Salad, Ground Beef</h6></CardText>
-                            </CardBody>
-                        </Card>
+                                <CardBody>
+                                    <CardText>
+                                        <h6>Tomatoes, Cheese, Salad, Ground Beef</h6></CardText>
+                                </CardBody>
+                            </Card>
                         </Col>
-                        <Col className="food-col">
-                            <Card className = "food-card">
-                            <CardImg top src={require("../../../assets/img/Hamburger.jpg")} alt="..." />
-                            <CardTitle>
-                                Hamburger XL
+                        <Col lg={4} md={4} sm={4} className="food-col">
+                            <Card className="food-card">
+                                <CardImg top src={require("../../../assets/img/Hamburger.jpg")} alt="..." />
+                                <CardTitle>
+                                    Hamburger XL
                             </CardTitle>
-                            <CardBody>
-                                <CardText>
-                                <h6>Tomatoes, Cheese, Salad, Ground Beef</h6></CardText>
-                            </CardBody>
-                        </Card>
+                                <CardBody>
+                                    <CardText>
+                                        <h6>Tomatoes, Cheese, Salad, Ground Beef</h6></CardText>
+                                </CardBody>
+                            </Card>
                         </Col>
                     </Row>
                 </Container>
+                <div className="pagination">
+                    <nav aria-label="Page navigation example">
+                        <Pagination>
+                            <PaginationItem className="disabled">
+                                <PaginationLink href="#pablo" onClick={e => e.preventDefault()}>
+                                    Previous
+            </PaginationLink>
+                            </PaginationItem>
+                            <PaginationItem className="active">
+                                <PaginationLink href="#pablo" onClick={e => e.preventDefault()}>
+                                    1
+            </PaginationLink>
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationLink href="#pablo" onClick={e => e.preventDefault()}>
+                                    2
+            </PaginationLink>
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationLink href="#pablo" onClick={e => e.preventDefault()}>
+                                    3
+            </PaginationLink>
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationLink href="#pablo" onClick={e => e.preventDefault()}>
+                                    Next
+            </PaginationLink>
+                            </PaginationItem>
+                        </Pagination>
+                    </nav>
+                </div>
             </div>
         </>
     )
