d003ca9e88de9c922eb162273af026261492ef1d
freund-admin <hr@freund.ba>
2020-01-08 12:36:00 +0100

    redux into pkg.json

--- client/src/components/order/order-total/OrderTotal.js
+++ client/src/components/order/order-total/OrderTotal.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './order-total.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faShoppingCart} from '@fortawesome/free-solid-svg-icons'
+import {faShoppingCart, faTrash} from '@fortawesome/free-solid-svg-icons'
 import {Button, Badge} from 'reactstrap'
 import store from "../../../redux/store/index"
 import { addItemToCart } from "../../../redux/actions/index"
@@ -26,20 +26,20 @@ function OrderTotal(){
     //const [orderList, setOrderList] = useState([])
     const orderList = useSelector(state => state.orderCart)
 
+    const orderItems = orderList.map((itemName) => 
+    <>
+    <Button color="info" className="order-item">{itemName}<FontAwesomeIcon className="remove-icon" icon={faTrash}></FontAwesomeIcon></Button>
+    <br></br>
+    </>
+    )
+
     return (
         <>
         <div className="container">
             <h4>Your Order:</h4>
 
             <div className="order-list">
-                <ul>    
-                    <li onClick={() => {console.log(orderList)}}>{orderList}</li>
-                    <li>Hamburger XL --- 1</li>
-                    <li>Hamburger XL --- 1</li>
-                    <li>Hamburger XL --- 1</li>
-                    <li>Hamburger XL --- 1</li>
-                    <li>Hamburger XL --- 1</li>
-                </ul>
+                {orderItems}
             </div>
                     <Button color="danger" size="lg" className="buy-icon">
                     <FontAwesomeIcon icon={faShoppingCart}/> 5 Items
